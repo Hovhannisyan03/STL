de06828c8ff7ad47d9515c0edd8c546216f16a63
Sargis <138877136+Hovhannisyan03@users.noreply.github.com>
2024-08-30 21:39:58 +0400

    Update deque.h

--- containers/deque/deque.h
+++ containers/deque/deque.h
@@ -4,10 +4,64 @@
 #include <iostream>
 #include <initializer_list>
 #include <memory>
-
+#include <new>
+#include <utility>
 namespace my_deque
 {
-    template <class T,  class Allocator = std::allocator<T>>
+
+    template <typename T>
+    class DequeAllocator 
+    {
+    public:
+        using value_type = T;
+
+        DequeAllocator() noexcept = default;
+
+        template <typename U>
+        DequeAllocator(const DequeAllocator<U>&) noexcept {}
+
+        T* allocate(std::size_t n) 
+        {
+            if (n == 0) return nullptr;
+            void* ptr = std::malloc(n * sizeof(T));
+            if (!ptr) throw std::bad_alloc();
+            return static_cast<T*>(ptr);
+        }
+
+        void deallocate(T* p, std::size_t n) noexcept 
+        {
+            std::free(p);
+        }
+
+        template <typename U, typename... Args>
+        void construct(U* p, Args&&... args) 
+        {
+            new (p) U(std::forward<Args>(args)...);
+        }
+
+        template <typename U>
+        void destroy(U* p) noexcept 
+        {
+            p->~U();
+        }
+
+        template <typename U>
+        struct rebind {
+            using other = DequeAllocator<U>;
+        };
+    };
+
+    template <typename T, typename U>
+    bool operator==(const DequeAllocator<T>&, const DequeAllocator<U>&) noexcept {
+        return true;
+    }
+
+    template <typename T, typename U>
+    bool operator!=(const DequeAllocator<T>& lhs, const DequeAllocator<U>& rhs) noexcept {
+        return !(lhs == rhs);
+    }
+
+    template <class T,  class Allocator = DequeAllocator<T>>
     class deque
     {
         public:
@@ -18,7 +72,7 @@ namespace my_deque
             using const_reference = const T&;
             using pointer = T*;
             using const_pointer = const T*;
-
+            using bf_type = double;
         public:
             class iterator;
             class const_iterator;
@@ -34,9 +88,9 @@ namespace my_deque
             deque(const deque& other, const Allocator& alloc);
             deque(deque&& other) noexcept;
             deque(deque&& other, const Allocator& alloc) noexcept;
-            deque (std::initializer_list<T> init, const Allocator& alloc = Allocator());
-            // template<typename InputIt>
-            // deque(InputIt first, InputIt last);
+            deque(std::initializer_list<T> init, const Allocator& alloc = Allocator());
+            template<typename InputIt>
+            deque(InputIt first, InputIt last);
             ~deque();
             allocator_type get_allocator() const;
 
@@ -46,11 +100,9 @@ namespace my_deque
             deque& operator=(std::initializer_list<value_type> init);
             
             void assign(size_type count, const value_type& val);
-            // template<class InputIt>
-            // void assign(InputIt first, InputIt last);
+            template<class InputIt>
+            void assign(InputIt first, InputIt last);
             void assign(std::initializer_list<value_type> init); 
-
-            // Allocator ----------------------
 
         public:
             reference at(size_type pos);
@@ -89,20 +141,19 @@ namespace my_deque
         public:
             void clear();
 
-            // iterator insert(const_iterator pos, const value_type& value);
+            iterator insert(const_iterator pos, const value_type& value);
             // iterator insert(const_iterator pos, value_type&& value);
             // iterator insert(const_iterator pos,size_type count, const value_type& value);
             // template<class InputIt>
             // iterator insert(const_iterator pos, InputIt first, InputIt last);
             // iterator insert(const_iterator pos, std::initializer_list<T> ilist);
 
+
             // template< class... Args >
             // iterator emplace(const_iterator pos, Args&&... args);
 
-            // iterator erase(iterator pos);
-            // iterator erase(const_iterator pos);
-            // iterator erase(iterator first, iterator last);
-            // iterator erase(const_iterator first, const_iterator last);
+            iterator erase(const_iterator pos);
+            iterator erase(const_iterator first, const_iterator last);
 
             void push_back(const value_type& value);
             void push_back(value_type&& value);
@@ -127,14 +178,15 @@ namespace my_deque
 
         private:
             void reserve(size_type newCapacity, bool left = true);
+            bool balance_factor() const;
+            void rebalance();
+           
             value_type *m_left;
             value_type *m_right;
             size_type m_l_size;
             size_type m_r_size;
             size_type m_l_capacity;
             size_type m_r_capacity;
-            size_type m_l_free;
-            size_type m_r_free;
             Allocator m_alloc;
     };
 }
@@ -148,12 +200,10 @@ class my_deque::deque<T,Allocator>::const_iterator
         friend class deque<T,Allocator>;
     protected:
         pointer m_ptr;        
-        pointer m_left;       
-        pointer m_right;      
-        size_type m_l_size; 
-        size_type m_r_size; 
+        pointer m_left;
+        pointer m_right;
     private:
-        const_iterator(pointer ptr = nullptr, pointer left = nullptr, pointer right = nullptr, size_type l_size = 0, size_type r_size = 0);
+        const_iterator(pointer ptr = nullptr, pointer left = nullptr, pointer right = nullptr);
     public:
         const_iterator() = default;
         const_iterator(const const_iterator&) = default;      
@@ -162,7 +212,7 @@ class my_deque::deque<T,Allocator>::const_iterator
         const_iterator& operator=(const const_iterator&) = default;      
         const_iterator& operator=(const_iterator&&) = default;
 
-        const_iterator operator+(size_type n) const;      
+        // const_iterator operator+(size_type n) const;      
         // const_iterator operator-(size_type n) const;
 
         const_iterator& operator++();
@@ -173,7 +223,7 @@ class my_deque::deque<T,Allocator>::const_iterator
         const_reference operator*() const;
         const_pointer operator->() const;
 
-        const_reference operator[](size_type pos) const;
+        // const_reference operator[](size_type pos) const;
 
         bool operator==(const const_iterator& other) const;      
         bool operator!=(const const_iterator& other) const;      
@@ -188,7 +238,7 @@ class my_deque::deque<T,Allocator>::iterator : public my_deque::deque<T,Allocato
 {
         friend class deque<T,Allocator>;
     private:
-        iterator(pointer ptr = nullptr, pointer left = nullptr, pointer right = nullptr, size_type l_size = 0, size_type r_size = 0);
+        iterator(pointer ptr = nullptr, pointer left = nullptr, pointer right = nullptr);    
     public:
         iterator() = default;
         iterator(const iterator&) = default;      
@@ -197,7 +247,7 @@ class my_deque::deque<T,Allocator>::iterator : public my_deque::deque<T,Allocato
         iterator& operator=(const iterator&) = default;      
         iterator& operator=(iterator&&) = default;
 
-        iterator operator+(size_type n) const;      
+        // iterator operator+(size_type n) const;      
         // iterator operator-(size_type n) const;
 
         iterator& operator++();
@@ -208,7 +258,7 @@ class my_deque::deque<T,Allocator>::iterator : public my_deque::deque<T,Allocato
         reference operator*();
         pointer operator->();
 
-        reference operator[](size_type pos) const;
+        // reference operator[](size_type pos) const;
 };
 
 
